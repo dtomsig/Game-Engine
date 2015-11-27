78c33a966f73d81107d471580ca58427b4d1f5b1
dtomsig <dtomsig@gmail.com>
2015-11-27 13:27:58 -0500

    Author: Diego Tomsig

--- src/Client.java
+++ src/Client.java
@@ -1,10 +1,23 @@
 import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
 
-public class Client 
+public class Client extends SimpleApplication
 {
     public static void main(String[] args) 
     {
-        System.out.println("gwas");
+        Client app = new Client();
+        app.start();
+    }
+    
+    public void simpleInitApp()
+    {
+        Box b = new Box(1, 1, 1); // create cube shape
+        Material mat = new Material(assetManager, "/Common/MatDefs/Misc/Unshaded.j3md");
+        Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape
+        geom.setMaterial(mat);
+        rootNode.attachChild(geom);              // make the cube appear in the scene
     }
 
 }
