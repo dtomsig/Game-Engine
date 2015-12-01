e54aa9b9ddb13acc097a504236174f32081ad367
dtomsig <dtomsig@gmail.com>
2015-12-01 11:35:27 -0500

    Author: Diego Tomsig

--- src/Client.java
+++ src/Client.java
@@ -1,30 +1,55 @@
 import com.jme3.app.Application;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 import com.jme3.system.AppSettings;
 
 public class Client extends Application
 {
     private AppSettings settings = new AppSettings(true);
+    protected Node rootNode = new Node("Root Node");
+    state clientState;
     
-    
-    public enum clientState
-    {
-        MAIN_MENU, IN_GAME, SCORE_SCREEN;
+    public enum state
+    {   
+        MAIN_MENU(0), IN_GAME(1), SCORE_SCREEN(2);
+        private int value;
+        
+        private state(int value)
+        {
+            this.value = value;
+        }
+        
+        int getValue()
+        {
+            return this.value;
+        }
     }
     
     public Client()
     {
-        settings.setTitle("Elements");
+        this.clientState = state.MAIN_MENU;
+       settings.setTitle("Elements");
         this.setSettings(settings);
         this.start();
     }
     
-    public void changeTitle()
+    public void changeTitle(String newTitle)
     {
-        
+        settings.setTitle(newTitle);
+        this.setSettings(settings);
+        this.restart();
+    }
+    
+    public void loadModels()
+    {
+        //Spatial test = assetManager.loadModel("Models/test.blend");
+        //viewPort.attachScene(rootNode   );
+      //  rootNode.attachChild(test);
     }
     
     public static void main(String[] args) 
     {
-        Client mainClient = new Client();
+        Client mainClient = new Client();     
+        mainClient.loadModels();
     }
 }
--- src/Unit.java
+++ src/Unit.java
@@ -2,8 +2,8 @@ public abstract class Unit
 {
     private int health;
     
-    public void reduceHealth(int incomingHealth)
+    public void reduceHealth(int incomingDamage)
     {
-        this.health -= incomingHealth;
+        this.health -= incomingDamage;
     }
 }
