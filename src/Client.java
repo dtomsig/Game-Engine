e03460f9264aec44a977463e63548d7372bfbfae
dtomsig <dtomsig@gmail.com>
2015-12-12 17:43:27 -0500

    Author: Diego Tomsig

--- src/Client.java
+++ src/Client.java
@@ -3,14 +3,18 @@ import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.glfw.GLFWKeyCallback;
 import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryUtil.NULL;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.system.MemoryUtil;
 
 public class Client
-{    
+{   
+    private int glfwErrorState; 
     private long windowHandle;
     private static GLFWErrorCallback errorCallback = GLFWErrorCallback.createPrint(System.err);
+    private state clientState;
+    
+    
     public enum state
     {   
         MAIN_MENU(0), IN_GAME(1), SCORE_SCREEN(2);
@@ -23,13 +27,12 @@ public class Client
         
         int getValue()
         {
-            return value;
+            return this.value;
         }
     }
     
     public Client()
     {
-        this.run();
     }
     
     public void run()
@@ -38,11 +41,11 @@ public class Client
         {
             initClient();
             update();
-            glfwDestroyWindow(windowHandle);
+            GLFW.glfwDestroyWindow(windowHandle);
         }
         finally
         {
-            glfwTerminate();
+            GLFW.glfwTerminate();
             errorCallback.release();
         }
         
@@ -50,37 +53,67 @@ public class Client
     
     public void initClient()
     {   
-        int glfwErrorState = glfwInit();
+        glfwErrorState = GLFW.glfwInit();
+        GLFW.glfwSetErrorCallback(errorCallback = GLFWErrorCallback.createPrint(System.err));
         
-        glfwSetErrorCallback(errorCallback = GLFWErrorCallback.createPrint(System.err));
-        
-        if(glfwErrorState == GL_FALSE)
+        if(glfwErrorState == GL11.GL_FALSE)
             throw new IllegalStateException("Failed to initialize.");
         
+        this.windowHandle = GLFW.glfwCreateWindow(640, 480, "Elements Game", 
+                                                  MemoryUtil.NULL, 
+                                                  MemoryUtil.NULL);
+        this.clientState = state.MAIN_MENU;
         
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        /* Prepares the client window */
+        GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE);
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_TRUE);
+        GLFWVidMode vidmode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+        GLFW.glfwMakeContextCurrent(windowHandle);
+        GLFW.glfwSwapInterval(1);
+        GLFW.glfwShowWindow(windowHandle);
+        GL.setCapabilities(GL.createCapabilities());
         
-        this.windowHandle = glfwCreateWindow(640, 480, "Elements Game", NULL, NULL);
-        GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-        glfwMakeContextCurrent(windowHandle);
-        glfwSwapInterval(1);
-        glfwShowWindow(windowHandle);
+        GL11.glViewport(0, 0, 640, 480);
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+        GL11.glLoadIdentity();
+        GL11.glOrtho(0.0, 10.0, 0.0, 10.0, -1.0, 1.0);
     }
     
     public void update()
-    {    
-        GL.createCapabilities();
-        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    {   
+        GL11.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         
-        while(glfwWindowShouldClose(windowHandle) == GLFW_FALSE)
+        // Main display loop.
+        while(GLFW.glfwWindowShouldClose(windowHandle) == GLFW.GLFW_FALSE)
         {
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            glfwSwapBuffers(windowHandle);
-            glfwPollEvents();
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+            
+            switch(clientState)
+            {
+                case MAIN_MENU:
+                    this.drawSquare();
+                    
+                case IN_GAME:
+                    
+            }
+            
+            GLFW.glfwSwapBuffers(windowHandle);
+            GLFW.glfwPollEvents();
         }
     }
     
+    public void drawSquare()
+    {        
+        GL11.glColor3f(0.8f, 0.3f, 0.6f);
+        GL11.glBegin(GL11.GL_POLYGON);
+        GL11.glVertex3f(2.0f, 4.0f, 0.0f);
+        GL11.glVertex3f(8.0f, 4.0f, 0.0f);
+        GL11.glVertex3f(8.0f, 6.0f, 0.0f);
+        GL11.glVertex3f(2.0f, 6.0f, 0.0f); 
+        GL11.glEnd();
+        //GL11.glFLush();
+    }
     
     public void changeTitle()
     {
--- src/ClientLoader.java
+++ src/ClientLoader.java
@@ -5,5 +5,6 @@ public static void main(String[] args)
         System.out.println("Running LWJGL native libraries.");
         System.setProperty("org.lwjgl.librarypath", "lib/native");
         Client c = new Client();
+        c.run();
     }
 }
